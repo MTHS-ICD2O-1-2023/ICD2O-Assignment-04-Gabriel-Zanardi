035b39865352f8c5bc612dac36c8ed2c34a17478
Gabriel Zanardi Nogueira <159185296+Gabriel-Zanardi@users.noreply.github.com>
2024-04-29 01:52:38 +0000

    edited html to change the topping format and init js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,27 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Gabriel
+// Created on: Mar 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function myButtonClicked() {
+  const TAX_RATE = 0.18
+
+  // input
+  const hoursWorked = document.getElementById("hours-worked").value
+  const hourlyRate = document.getElementById("hourly-rate").value
+
+  // process
+  const pay = (hoursWorked * hourlyRate) * (1.00 - TAX_RATE)
+  const roundedAnswer = pay.toFixed(2)
+  const payTwo = (hoursWorked * hourlyRate) * TAX_RATE
+  const roundedAnswerTwo = payTwo.toFixed(2)
+
+  // output
+  document.getElementById("income-salary").innerHTML =
+    "your pay is: CAD$" + roundedAnswer 
+    document.getElementById("government-tax").innerHTML = 
+    "the government will take: CAD$" + roundedAnswerTwo
+}
