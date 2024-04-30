f73483b3a85304998c87f83572ee0131b2e45003
Gabriel Zanardi Nogueira <159185296+Gabriel-Zanardi@users.noreply.github.com>
2024-04-30 01:37:10 +0000

    check with the teacher tmrw

--- js/script.js
+++ js/script.js
@@ -8,37 +8,43 @@ function myButtonClicked() {
   const numberToppings = document.getElementById("number-toppings").value;
 
   // process
-  let totalCost;
+let totalCost;
 
-  if (pizzaSize == "extra large") {
-    totalCost = 10.00;
-    
-    if (numberToppings == "1") {
-      totalCost += 1.00;
-    } else if (numberToppings == "2") {
-      totalCost += 1.75;
-    } else if (numberToppings == "3") {
-      totalCost += 2.50;
-    } else if (numberToppings == "4") {
-      totalCost += 3.35;
-    }
-  else if (pizzaSize == "large") {
-    totalCost = 6.00
-    if (numberToppings == "1") {
-      totalCost += 1.00;
-    } else if (numberToppings == "2") {
-      totalCost += 1.75;
-    } else if (numberToppings == "3") {
-      totalCost += 2.50;
-    } else if (numberToppings == "4") {
-      totalCost += 3.35;
-    }
+if (pizzaSize == "extra large") {
+  totalCost = 10.00;
+  
+  if (numberToppings == "1") {
+    totalCost += 1.00;
+  } else if (numberToppings == "2") {
+    totalCost += 1.75;
+  } else if (numberToppings == "3") {
+    totalCost += 2.50;
+  } else if (numberToppings == "4") {
+    totalCost += 3.35;
+  }
+} else if (pizzaSize == "large") {
+  totalCost = 6.00;
+  
+  if (numberToppings == "1") {
+    totalCost += 1.00;
+  } else if (numberToppings == "2") {
+    totalCost += 1.75;
+  } else if (numberToppings == "3") {
+    totalCost += 2.50;
+  } else if (numberToppings == "4") {
+    totalCost += 3.35;
   }
 }
 
-  const noTaxCost = totalCost;
+ const noTaxCost = (totalCost * (1.00 - TAX_RATE)).toFixed(2);
+  const governmentTax = (totalCost * TAX_RATE).toFixed(2);
+  const totalAmount = (parseFloat(governmentTax) + parseFloat(noTaxCost)).toFixed(2);
 
-  // output
-  document.getElementById("cost-no-tax").innerHTML =
-    "your cost with no tax is: CAD$" + noTaxCost;
+// Output
+document.getElementById("cost-no-tax").innerHTML =
+  "Your cost with no tax is: CAD$" + noTaxCost;
+document.getElementById("government-tax").innerHTML =
+  "The government tax on your purchase is: CAD$" + governmentTax;
+document.getElementById("total-amount").innerHTML =
+  "The total amount on your purchase is: CAD$" + totalAmount;
 }
